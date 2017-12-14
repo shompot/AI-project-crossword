54c3a596fcb7285c7c01c6572d8505a629aafa11
shompot <cholpon94@gmail.com>
2017-12-14 04:09:50 +0300

    log kinda added (maybe)

--- src/SolvePuzzle.java
+++ src/SolvePuzzle.java
@@ -10,6 +10,8 @@ public class SolvePuzzle {
     private Crossword crossword;
     private ArrayList<String> clues;
     private ArrayList<String> solution;
+    private ArrayList<String> logs;
+    //private String
 
     // CONSTRUCTOR
     public SolvePuzzle(){
@@ -17,27 +19,31 @@ public class SolvePuzzle {
         wordLists = new ArrayList<ArrayList<String>>();
         clues = new ArrayList<String>();
         solution = new ArrayList<String>();
+        logs= new ArrayList<String>();
+        logs.add("Solve Puzzle object is created");
     }
 
     // SETTERS
 
     // GETTERS
+    public ArrayList<String> getLog() { return logs; }
 
     // METHODS
     public void openCrossword () throws IOException{
+        logs.add("Retrieving crossword");
         crossword.readGridFromFile("crosswords/December 12, 2017.html");
         //crossword.readGridFromUrl();
     }
    public void readWordLists () throws IOException{
-
-
+        logs.add("Getting Clues from the Crossword");
         // get clues
         clues.addAll(crossword.getDownHintsOnly());
         clues.addAll(crossword.getAcrossHintsOnly());
 
+        logs.add("Getting Solution from the Crossword");
         // get solution
-       solution.addAll(crossword.getDownSolution());
-       solution.addAll(crossword.getAcrossSolution());
+        solution.addAll(crossword.getDownSolution());
+        solution.addAll(crossword.getAcrossSolution());
 
        // create searching instances
         GoogleSearch googleSearch = new GoogleSearch();
@@ -48,12 +54,17 @@ public class SolvePuzzle {
         // search each clue one by one
        for (int i=0; i <clues.size(); i ++){
            ArrayList<String> result = new ArrayList<String>();
-           //String[] words = clues.get(i).split("\\s+");
-           for (String word: clues.get(i).split("\\s+")) {
-               result.addAll(theSaurusSearch.search(word, solution.get(i).length()));
+           String[] words = clues.get(i).split("\\s+");
+           //for (String word: clues.get(i).split("\\s+")) {
+            if (words.length==1){
+                logs.add("Searching in TheSaurus");
+               result.addAll(theSaurusSearch.search(words[0], solution.get(i).length()));
            }
+           logs.add("Searching in Dictionary");
            result.addAll(dictSearch.search(clues.get(i), solution.get(i).length()));
+           logs.add("Searchng in Google");
            result.addAll(googleSearch.search(clues.get(i), solution.get(i).length()));
+           logs.add("Searching in Lyrics");
            result.addAll(lyricsSearch.search(clues.get(i), solution.get(i).length()));
 
            wordLists.add(result);
@@ -62,9 +73,9 @@ public class SolvePuzzle {
    }
 
     public char[][] solve() throws IOException{
-
         openCrossword();
         readWordLists();
+        logs.add("Trying the retrieved words");
         char [] solutionArr = crossword.getSolutionArr();
 
         char [][] answer = new char [5][5];
